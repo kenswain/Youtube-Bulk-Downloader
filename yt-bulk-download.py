39bd786be305bf5cd9b82597088e2e9818673d1d
Ken Swain <kenswain@Kens-MBP.attlocal.net>
2020-12-30 12:14:08 -0600

    Added Checking for Videos.txt file

--- yt-bulk-download.py
+++ yt-bulk-download.py
@@ -1,11 +1,18 @@
 #!/usr/local/bin/python3
 
-import os
+import os, time
+def checkfile():
+    if not os.path.exists('videos.txt'):
+        command = 'touch videos.txt'
+        os.popen(command)
+        time.sleep(.5)
+
+checkfile()
 filepath = 'videos.txt'
 with open(filepath) as fp:
     line = fp.readline()
     cnt = 1
-    os.chdir("{youtdir}")
+    os.chdir("{Your Downloads Dir}")
     
     while line:
 #        print("line {}: {}".format(cnt, line.strip()))
